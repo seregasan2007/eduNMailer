f7e37b1f7f712d87ee96028476e742a920666935
seregasan2007 <39331524+seregasan2007@users.noreply.github.com>
2019-02-07 14:07:53 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
 const nodemailer = require('nodemailer');
 
-const xoauth2 = require('xoauth2');
-
-let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        xoauth2: xoauth2.createXOAuth2Generator({
-            user: 'mama210897@gmail.com',
-            clientId: '164338413337-t7mvh8cahqvg1jua0ji0hv20uuj7idc8.apps.googleusercontent.com',
-            clientSecret: 'ty pidor',
-            refreshToken: '1/orlgGx2JC8zRPs03dQJuPcByQeJjqIpCfASVWohZZvTf_5b8Lxyb4OHQFez3ym8G'
-        })
-    }
+var email_smtp = nodemailer.createTransport({ 
+host: "smtp.gmail.com",
+auth: {
+type: "login", // default
+user: "login@gmail.com", // <— сюда логин
+pass: "passwod" // <— сюда павроль
+}
 });
 
 let HelperOptions = {
-    from: '"Сергей Чернецов" <mama210897@gmail.com',
-    to: 'trashtube2323@gmail.com',
-    subject: 'Hello World!',
-    text: 'You are gay'
+from: '"Сергей Чернецов" <mama210897@gmail.com',
+to: 'trashtube2323@gmail.com',
+subject: 'Hello World!',
+text: 'Lorem text'
 };
 
-transporter.sendMail(HelperOptions, (error, info) => {
-    if(error){
-        return console.log(error);
-    }
-    console.log('The message was sent! eeeeeeeeeeee');
-    console.log(info);
+email_smtp.sendMail(HelperOptions, (error, info) => {
+if(error){
+return console.log(error);
+}
+console.log('The message was sent! eeeeeeeeeeee');
+console.log(info);
 })
